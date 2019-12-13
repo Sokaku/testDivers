c6e615087f4f80ab8fe0474f38091c3b9741818e
Puneet <puneet.dave@t-systems.com>
2019-12-13 10:45:52 +0100

    enorme

--- testDivers/src/testDivers/MainTestDives.java
+++ testDivers/src/testDivers/MainTestDives.java
@@ -13,7 +13,7 @@ public class MainTestDives {
    public static void main(String[] args) {
       // TODO Auto-generated method stub
       SonClass sc = new SonClass();
-      System.out.print("XXXXXX");
+      System.out.print("XXXpeneXXX");
    }
 
 }
