7b91fd82a23b31a02f2ca3574e5d436712b5190e
artzain <gorkamunoz@gmail.com>
2019-12-13 10:49:11 +0100

    Nintendo-DS

---
+++ testDivers/src/testDivers/GladiatorClass.java
@@ -0,0 +1,27 @@
+package testDivers;
+//*****************************************************************************
+//* THIS PROGRAM CONTAINS PROPRIETARY INFORMAION WHICH IS TRADE SECRET OF     *
+//* T-SYSTEMS, DEBIS SYSTEMHAUS AND ALSO IS PROTECTED AS AN UNPULISHED WORK   *
+//* UNDER APPLICABLE COPYRIGHT LAWS. THE PROGRAM IS TO BE RETAINED IN         *
+//* CONFIDENCE. ANY USE BY THIRD PARTIES (E.G. USE AS A CONTROL PROGRAM,      *
+//* REPRODUCTION, MODIFICATION AND TRANSLATION) IS GOVERNED SOLELY BY WRITTEN *
+//* AGREEMENTS WITH T-SYSTEMS, DEBIS SYSTEMHAUS.                              *
+// ****************************************************************************
+
+public class GladiatorClass {
+
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+      System.out.print("¿Te gustan las pelis de gladiadores?");
+   }
+
+}
+
+
+// Configuration Management Information:
+// -------------------------------------
+// $Id
+//
+// Version History:
+// ----------------
+// $Log
